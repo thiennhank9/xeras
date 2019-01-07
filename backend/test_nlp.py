b9fc96af0e6c2cb0368966597b72c0f52316e5ca
thiennhank9 <thiennhank9@gmail.com>
2019-01-07 21:35:37 +0700

    #Change + to plus to avoid bug from using reg

--- backend/test_nlp.py
+++ backend/test_nlp.py
@@ -1,16 +1,24 @@
+import re
 from xeras.nlp.nlp import NLP
 from xeras.nlp.same_words import SameWords
 
 
 SAMPLE_SENTENCE = 'Bản màu vàng còn hàng ở Q9 TPHCM ko shop'
+TEST_SAME_WORD = "Iphone 7 có đang khuyến mãi ko?"
 
-
-if __name__ == '__main__':
+def test_accuracy():
     nlp = NLP()
     # nlp.set_is_used_model(True)
     nlp.setup()
     nlp.test_accuracy()
+    print(nlp.get_predict(SAMPLE_SENTENCE))
+
+def test_same_word():
     same_words = SameWords()
     same_words.load_same_words()
-    print(same_words.replace_same_word("xs max la dien thoai"))
-    # print(nlp.get_predict(SAMPLE_SENTENCE))
+    print(TEST_SAME_WORD)
+    print(same_words.replace_same_word(TEST_SAME_WORD))
+
+if __name__ == '__main__':
+    test_accuracy()
+    # test_same_word()
