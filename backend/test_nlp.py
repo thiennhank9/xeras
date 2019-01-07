154ca012315e9675b3e8423747261c05c8e3d09c
nhanthiennguyen <nhanthiennguyen@kms-technology.com>
2019-01-07 13:06:19 +0700

    #Fix lan 1 same word, ready to test more

--- backend/test_nlp.py
+++ backend/test_nlp.py
@@ -1,4 +1,5 @@
 from xeras.nlp.nlp import NLP
+from xeras.nlp.same_words import SameWords
 
 
 SAMPLE_SENTENCE = 'Bản màu vàng còn hàng ở Q9 TPHCM ko shop'
@@ -9,4 +10,7 @@ if __name__ == '__main__':
     # nlp.set_is_used_model(True)
     nlp.setup()
     nlp.test_accuracy()
+    same_words = SameWords()
+    same_words.load_same_words()
+    print(same_words.replace_same_word("xs max la dien thoai"))
     # print(nlp.get_predict(SAMPLE_SENTENCE))
--- backend/xeras/nlp/same_words.py
+++ backend/xeras/nlp/same_words.py
@@ -24,45 +24,71 @@ class SameWords:
                 # Lower and remove front and back spaces
                 other_word = other_word.lower().strip()
 
-                # Add to dictionary. EX: same_words['ip X'] = Iphone X
+                # Don't append if other word is as same as true word, to prevent bugs
+                if other_word == true_word:
+                    continue
+
+                # Add to dictionary. EX: same_words_dict['ip X'] = Iphone X
                 self.same_words_dict[other_word] = true_word
                 self.same_words_array.append((len(other_word), other_word, true_word))
-
+        
+        # Remove duplicates
         self.same_words_array = list(set(self.same_words_array))
+        # Sort by other word by length, descending
         self.same_words_array.sort(key=get_other_word_len, reverse=True)
     
     def replace_same_word(self, sentence=Settings.SAMPLE_SENTENCE):
         sentence = sentence.lower().strip()
         sentence = re.sub('[!@#$]', '', sentence)
-        replaced_index = []
-        replaced_word = []
+        replaced_words = []
+        replaced_indexs = []
 
         for same_words_ele in self.same_words_array:
             (other_word_len, other_word, true_word) = same_words_ele
             re_finditer = re.finditer(r'\b{0}\b'.format(other_word), sentence)
 
             if re_finditer:
-                for match in re_finditer:
-                    for (word in replaced_word):
-                        
+                plus = 0
 
-                    plus = 0
-                    for (index_previous_start, index_previous_end) in replaced_index:
-                        if (index_previous_start <= match.start() and match.start() < index_previous_end) 
-                        or (index_previous_start <= match.end() and match.end() < index_previous_end)
-                        or (match.start() <= index_previous_start and index_previous_end <= match.end()):
-                            continue
-                        
-                        sentence = re.sub(r'\b{0}\b'.format(other_word), true_word, sentence, 1)
-                        plus += (len(true_word) - len(other_word))
+                for match_other_word in re_finditer:
+                    # Get the previous index replaced
+                    for replaced_word in replaced_words:
+                        word_indexs = re.finditer(r'\b{0}\b'.format(replaced_word), sentence)
+                        if word_indexs:
+                            for word_index in word_indexs:
+                                replaced_indexs.append((word_index.start(), word_index.end()))
 
-                    # if index_start:
-                    #     print((index_start))
+                    is_in = False
 
-        if self.is_used_same_words:
-            for other_word, true_word in self.same_words_dict.items():
-                # Replace other word with the true word defined
-                if other_word in sentence:
-                    sentence = re.sub(r'\b{0}\b'.format(other_word), true_word, sentence)
+                    index_start = match_other_word.start() + plus
+                    index_end = match_other_word.end() + plus
+
+                    # Check if current other word is in replaced index
+                    for (index_previous_start, index_previous_end) in replaced_indexs:
+                        if (index_previous_start <= index_start and index_start <= index_previous_end) or (index_previous_start <= index_end and index_end <= index_previous_end) or (index_start <= index_previous_start and index_previous_end <= index_end):
+                            is_in = True
+                            break
+                    
+                    # If is in, then switch to the next match
+                    if is_in:
+                        continue
+
+                    # Replace true word at the index start
+                    sentence_start = ""
+                    sentence_end = ""
+
+                    if index_start != 0:
+                        sentence_start = sentence[:index_start-1]
+                    if index_end != (len(sentence) - 1):
+                        sentence_end = sentence[index_end+1:]
+
+                    sentence = sentence_start + true_word + " " + sentence_end
+
+                    # Append true word to replaced word
+                    replaced_words.append(true_word)
+                    replaced_words = list(set(replaced_words))
+
+                    replaced_indexs = []
+                    plus += (len(true_word) - len(other_word))
 
         return sentence
